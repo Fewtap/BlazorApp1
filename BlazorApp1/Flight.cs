66155114646294cf85d858acdd3a956c2c653247
fewtap9 <banansplit21@hotmail.com>
2022-12-05 08:44:50 -0300

    Add project files.

---
+++ BlazorApp1/Flight.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorApp1
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Flight
+    {
+        public string Rute { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime Planned { get; set; }
+        public DateTime? Estimated { get; set; }
+        public object Actual { get; set; }
+        public Status Status { get; set; }
+        public string FlightHash { get; set; }
+        public string ArrivalICAO { get; set; }
+        public string DepartureICAO { get; set; }
+    }
+
+    public class Status
+    {
+        public string kl { get; set; }
+        public string en { get; set; }
+        public string da { get; set; }
+    }
+
+
+}
