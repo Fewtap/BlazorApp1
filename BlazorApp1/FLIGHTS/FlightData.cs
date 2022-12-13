6d71e89292d6d02e7ca4a14b0bb360cfa3857928
fewtap9 <banansplit21@hotmail.com>
2022-12-13 14:44:53 -0300

    Added methods to post rooms and get flights

--- BlazorApp1/FLIGHTS/FlightData.cs
+++ BlazorApp1/FLIGHTS/FlightData.cs
@@ -17,44 +17,43 @@ namespace FlightData
 // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
     public class Flight
     {
-        
-
+        [JsonProperty("Rute")]
         public string Rute { get; set; }
+
+        [JsonProperty("DepartureAirport")]
         public string DepartureAirport { get; set; }
+
+        [JsonProperty("ArrivalAirport")]
         public string ArrivalAirport { get; set; }
+
+        [JsonProperty("Planned")]
         public DateTime Planned { get; set; }
+
+        [JsonProperty("Estimated")]
         public DateTime? Estimated { get; set; }
-        public string status_kl { get; set; }
-        public string status_en { get; set; }
-        public string status_da { get; set; }
+
+        [JsonProperty("status_kl")]
+        public string StatusKl { get; set; }
+
+        [JsonProperty("status_en")]
+        public string StatusEn { get; set; }
+
+        [JsonProperty("status_da")]
+        public string StatusDa { get; set; }
+
+        [JsonProperty("FlightHash")]
         public string FlightHash { get; set; }
+
+        [JsonProperty("ArrivalICAO")]
         public string ArrivalICAO { get; set; }
+
+        [JsonProperty("DepartureICAO")]
         public string DepartureICAO { get; set; }
-
-        
-
-        
-
-        public Flight()
-        {
-            
-            
-            
-            
-        }
-
-        
-
     }
 
 
 
-    public class Status
-    {
-        public string kl { get; set; }
-        public string en { get; set; }
-        public string da { get; set; }
-    }
+    
 
     public class FlightDB
     {
@@ -63,7 +62,7 @@ namespace FlightData
         
         
 
-        public static async Task<List<Flight>> GetFlights()
+        public static async Task<List<Flight>> GetFlights(DateTime date)
         {
             if (IsGettingData)
             {
@@ -73,29 +72,24 @@ namespace FlightData
             {
                 IsGettingData = true;
             }
-            string url = "https://www.mit.gl/wp-content/themes/mitgl/webservice.php?type=Departures&icao=BGJN";
+            string url = $"http://localhost:5018/api/flights/{date:yyyy-MM-dd}";
             RestClient client = new RestClient();
 
             RestRequest request = new RestRequest(url, Method.Get);
 
             RestResponse response = await client.ExecuteAsync(request);
 
+            // Create a JsonSerializerSettings object.
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+
+// Specify that null values should be ignored during serialization.
+            settings.NullValueHandling = NullValueHandling.Include;
 
 
-            var Flights = JsonConvert.DeserializeObject<List<Flight>>(response.Content);
+            var Flights = JsonConvert.DeserializeObject<List<Flight>>(response.Content, settings);
 
             
-            foreach (var flight in Flights)
-            {
-                flight.Planned = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(flight.Planned, "Greenland Standard Time");
-                if(flight.Estimated != null)
-                {
-                    flight.Estimated = TimeZoneInfo.ConvertTimeBySystemTimeZoneId((DateTime)flight.Estimated, "Greenland Standard Time");
-                }
-
-                Console.WriteLine(flight.Planned.ToShortTimeString());
-                
-            }
+            
             
             IsGettingData = false;
             return Flights.ToList();
@@ -105,10 +99,12 @@ namespace FlightData
         }
 
 
-        public static async Task PostRoom(Room data)
+        
+        
+        public static async Task PostRoom(List<Room> data)
         {
             // Set the URL of the API
-            var url = "http://localhost:5000/api/data";
+            var url = "http://localhost:5018/api/rooms/add";
 
             // Create a new RestClient
             var client = new RestClient();
@@ -120,7 +116,7 @@ namespace FlightData
             request.AddHeader("Content-Type", "application/json");
 
             // Serialize the struct to a JSON string
-            var json = JsonConvert.SerializeObject(data);
+            var json = JsonConvert.SerializeObject(data,Formatting.None);
 
             // Add the JSON string to the request body
             request.AddParameter("application/json", json, ParameterType.RequestBody);
@@ -129,7 +125,7 @@ namespace FlightData
             var response = await client.ExecuteAsync(request);
 
             // Print the response
-            Console.WriteLine(response.Content);
+            Console.WriteLine(json);
         }
 
         public async Task<List<Flight>> GetRooms(DateTime date)
